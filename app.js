b83b870c72a7e865488df3fd2be61441a1e6acbd
Siddhesh Naik <60619789+sidnaik13499@users.noreply.github.com>
2021-07-26 20:35:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
-
+if (window.location.protocol.indexOf('https') == 0){
+  var el = document.createElement('meta')
+  el.setAttribute('http-equiv', 'Content-Security-Policy')
+  el.setAttribute('content', 'upgrade-insecure-requests')
+  document.head.append(el)
+}
 
 function fetchGiphy(){
     var apiKey = "hhKSYOIPega6fQJYkfMBogdncYdxelCi";
